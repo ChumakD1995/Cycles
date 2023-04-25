42754a40da464a14b9cdac36ee722b5c9dd749e9
chumak8088@gmail.com <k.smulska@pro-capital.local>
2023-04-25 21:00:21 +0300

    Cycles

--- Cycles/index.js
+++ Cycles/index.js
@@ -1,14 +1,49 @@
-let arr = [];
-let length = prompt("Введіть довжину масиву:");
-for (let i = 0; i < length; i++) {
-  let element = prompt("Введіть елемент масиву:");
-  arr.push(element);
+for (let i = 10; i <= 20; i++) {
+  console.log(i);
+  if (i < 20); 
+}
+console.log(', ');
+
+for (let i = 10; i <= 20; i++) {
+  let square = i * i;
+  console.log(square);
 }
 
-arr.sort(function(a, b){return a-b}); 
 
-console.log("Відсортований масив: " + arr + "<br>");
+for (let i = 1; i <= 10; i++) {
+  console.log(" " + i + " * 7 =");
+  console.log(" " + (i * 7) + " ");
+}
 
-arr.splice(1, 3);
+let sum = 0;
+for (let i = 1; i <= 15; i++) {
+  sum += i; 
+}
+console.log(sum);
 
-console.log("Масив після видалення елементів з 2 по 4 (включно!): " + arr);
+let product = 1;
+for (let i = 15; i <=35; i++) {
+  product *= i;
+}
+console.log(product);
+
+let sum1 = 0;
+for (let i = 1; i <= 500; i++) {
+  const average = sum1 / 500;
+  sum1 += i;
+  console.log(average)
+}
+
+let sum2 = 0;
+for (let i = 30; i <= 80; i++) {
+  if (i % 2 === 0) {
+    sum2 += i;
+  }
+}
+console.log(sum2)
+
+for (let i = 100; i <= 200; i++) {
+  if (i % 3 === 0) {
+    console.log(i)
+  }
+}
